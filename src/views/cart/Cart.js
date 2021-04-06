f0fd31bac6e3dfcde3547bf9907b1950d4b21faf
ridhoajibx <ridhoajibx@gmail.com>
2021-04-07 02:35:08 +0700

    [ridho] - refactor & put reactive effect to carts

--- src/views/cart/Cart.js
+++ src/views/cart/Cart.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import Header from '../../components/Header'
 import App from '../../layouts/App'
@@ -9,11 +9,13 @@ import { Link } from 'react-router-dom'
 
 export default function Cart() {
     const [carts, setCarts] = useRecoilState(aNumberOfCart);
+    const [total, setTotal] = useState('');
 
     const removeCartHandler = async (index) => {
         try {
             const { data } = await axios.delete(`/api/remove-cart/${carts[index].id}`)
             setCarts(carts.filter(i => (i !== carts[index])));
+            setTotal(total - carts[index].price);
             toast(`🚀 ${data.message}`, {
                 position: "top-right",
                 autoClose: 3000,
@@ -26,57 +28,65 @@ export default function Cart() {
         }
     }
 
+    useEffect(() => {
+        let price = (carts.map(cart => cart.price))
+        setTotal(price.reduce((x, y) => x + y, 0))
+    }, [total, carts]);
+
     return (
-        <App title="Cart">
+        <App title="Carts">
             <ToastContainer />
-            <Header title="Cart">
+            <Header title="Carts">
                 Your cart
             </Header>
 
             <div className="container">
                 <div className="row">
-                    <div className="col-md-8" style={{ marginTop: -70 }}>
-                        <div className="card">
-                            <div className="card-header">
-                                Your cart
-                            </div>
-                            <div className="card-body">
-                                <table className="table">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">Playlist</th>
-                                            <th scope="col">Price</th>
-                                            <th scope="col">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        { carts.length > 0 ?
-                                            carts.map((cart, index) => (
+                    {carts.length > 0 ?
+                        <div className="col-md-8 mb-4">
+                            <div className="card">
+                                <div className="card-header d-flex align-items-center fw-bold">
+                                    <span className="badge bg-primary me-2">{carts.length}</span>
+                                    <span>Total</span>
+                                </div>
+                                <div className="card-body">
+                                    <table className="table">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Playlist</th>
+                                                <th scope="col" className="text-end">Price</th>
+                                                <th scope="col" className="text-end">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {carts.map((cart, index) => (
                                                 <tr key={index}>
                                                     <th scope="row">{index + 1}</th>
                                                     <td>{cart.playlist.name}</td>
-                                                    <td>Rp {cart.price}</td>
-                                                    <td>
+                                                    <td className="text-end">Rp {cart.price}</td>
+                                                    <td className="text-end">
                                                         <button onClick={() => { removeCartHandler(index) }} className="btn btn-danger btn-sm">Remove</button>
                                                     </td>
                                                 </tr>
-                                            )) :
-
+                                            ))
+                                            }
                                             <tr>
-                                                <th scope="row">1</th>
-                                                <th scope="row">You haven't got any playlist yet!</th>
-                                                <td>Rp 0</td>
-                                                <td>
-                                                    <Link to="/series" className="btn btn-primary btn-sm">Check Series</Link>
+                                                <td colSpan="2"></td>
+                                                <th className="text-end" scope="row">Rp {total}</th>
+                                                <td className="text-end">
+                                                    <button className="btn btn-primary btn-sm">Checkout</button>
                                                 </td>
                                             </tr>
-                                        }
-                                    </tbody>
-                                </table>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        </div> :
+                        <div className="alert alert-warning" role="alert">
+                            Your cart is empty. please buy atleast one, go to the <Link className="btn btn-sm btn-primary" to="/series"><i className="bi bi-collection-play me-2"></i>Series</Link>
+                      </div>
+                    }
                 </div>
             </div>
         </App >
